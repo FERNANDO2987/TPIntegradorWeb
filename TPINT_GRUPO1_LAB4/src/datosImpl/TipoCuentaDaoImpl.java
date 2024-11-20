0e03addff1af64e3faa54f5df16992b2307edca8
Fernando2987 <fernandopalacios51@gmail.com>
2024-11-20 10:19:48 -0300

    Add: ClienteDaoImpl

---
+++ TPINT_GRUPO1_LAB4/src/datosImpl/TipoCuentaDaoImpl.java
@@ -0,0 +1,37 @@
+package datosImpl;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import datos.TipoCuentaDao;
+import entidad.TipoCuenta;
+
+public class TipoCuentaDaoImpl implements TipoCuentaDao {
+	
+	private Conexion cn;
+	private final String querySeleccion = "Select * from tiposcuenta";
+	@Override
+	public List<TipoCuenta> obtenerCuentas() {
+		List<TipoCuenta> lista = new ArrayList<TipoCuenta>();
+		cn = new Conexion();
+		cn.Open();
+		
+		try
+		{
+			PreparedStatement prst = cn.connection.prepareStatement(querySeleccion);
+			prst.execute();
+			ResultSet rs = prst.getResultSet();
+			while(rs.next())
+			{
+				TipoCuenta aux = new TipoCuenta();
+				aux.setId(rs.getInt("id"));
+				aux.setDescripcion(rs.getString("descripcion"));
+				
+				lista.add(aux);
+			}
+                 
+		}
+	}
+}
